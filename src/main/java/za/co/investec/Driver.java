2d5d6389d13f2a493b00eee1008a5675440c404a
Msawenkosi Ntuli <msawenkosintuli@Msawenkosis-iMac.local>
2019-03-30 19:38:32 +0200

    Project structure, add value objects

---
+++ src/main/java/za/co/investec/Driver.java
@@ -0,0 +1,9 @@
+package za.co.investec;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ src/main/java/za/co/investec/vo/Address.java
@@ -0,0 +1,107 @@
+package za.co.investec.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Address implements Serializable {
+
+	private int id;
+	private Type type;
+	private AddressLineDetail addressLineDetail;
+	private ProvinceOrState provinceOrState;
+	private String cityOrTown;
+	private Country country;
+	private String postalCode;
+	private String suburbOrDistrict;
+	private Date lastUpdated;
+	
+	public int getId() {
+		
+		return id;
+	}
+	
+	public void setId(int id) {
+		
+		this.id = id;
+	}
+	
+	public Type getType() {
+		
+		return type;
+	}
+	
+	public void setType(Type type) {
+		
+		this.type = type;
+	}
+	
+	public AddressLineDetail getAddressLineDetail() {
+		
+		return addressLineDetail;
+	}
+	
+	public void setAddressLineDetail(AddressLineDetail addressLineDetail) {
+		
+		this.addressLineDetail = addressLineDetail;
+	}
+	
+	public ProvinceOrState getProvinceOrState() {
+		
+		return provinceOrState;
+	}
+	
+	public void setProvinceOrState(ProvinceOrState provinceOrState) {
+		
+		this.provinceOrState = provinceOrState;
+	}
+	
+	public String getCityOrTown() {
+		
+		return cityOrTown;
+	}
+	
+	public void setCityOrTown(String cityOrTown) {
+		
+		this.cityOrTown = cityOrTown;
+	}
+	
+	public Country getCountry() {
+		
+		return country;
+	}
+	
+	public void setCountry(Country country) {
+		
+		this.country = country;
+	}
+	
+	public String getPostalCode() {
+		
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		
+		this.postalCode = postalCode;
+	}
+	
+	public String getSuburbOrDistrict() {
+		
+		return suburbOrDistrict;
+	}
+	
+	public void setSuburbOrDistrict(String suburbOrDistrict) {
+		
+		this.suburbOrDistrict = suburbOrDistrict;
+	}
+	
+	public Date getLastUpdated() {
+		
+		return lastUpdated;
+	}
+	
+	public void setLastUpdated(Date lastUpdated) {
+		
+		this.lastUpdated = lastUpdated;
+	}	
+}
